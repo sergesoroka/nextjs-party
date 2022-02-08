c9db38f2ee25bce82523e036b87ef8263c770088
Sergey <sergey@Sergeys-iMac.local>
2022-02-08 12:23:11 +0200

    fixed missing module

--- pages/ob.js
+++ pages/ob.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Table from '../components/Table';
 import D3BarsPage from '../components/D3BarsPage';
-import { dataSlugi } from '../data/slugi_program';
+import { slugiPromises } from '../data/slugi_promises.js';
 import styles from '../styles/Home.module.css';
 
 export default function slugi() {
   return (
     <div className={styles.container}>
-        <main className={styles.main}>
-          <div className={styles.grid}>
-            <D3BarsPage />
-            <Table data={dataSlugi}/>
-          </div>
-        </main>
+      <main className={styles.main}>
+        <div className={styles.grid}>
+          <D3BarsPage />
+          <Table data={slugiPromises} />
+        </div>
+      </main>
     </div>
-    )
+  );
 }
